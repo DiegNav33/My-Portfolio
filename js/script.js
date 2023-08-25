e3a75500ed36b9e4c5ef11ad7ace77ea7e575bce
DiegNav33 <diegnavpro@gmail.com>
2023-08-26 00:32:02 +0200

    script added on all pages, header modified and with JS, scss modified

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
-document.addEventListener("DOMContentLoaded",function(){
-    console.log("Are you ready ?!");
+document.addEventListener("DOMContentLoaded", function() {
+
+    function swipeHeader() {
+        const headerElt = document.querySelector("header");
+        const headerLogoElt = document.querySelector("header img");
+        if (window.scrollY >= 100) {
+
+            headerElt.classList.remove("homeHeaderWhite");
+            headerElt.classList.add("homeHeaderBlack");
+            headerLogoElt.src="/images/Diego-logo-black-header.png";
+            headerLogoElt.style.width="50px";
+            
+        }
+
+        else if (window.scrollY <= 70) {
+
+            headerElt.classList.remove("homeHeaderBlack");
+            headerElt.classList.add("homeHeaderWhite");
+            headerLogoElt.src="/images/diego-logo-white-header.png";
+            headerLogoElt.style.width="80px";
+            
+        }
+    };
+
+    document.addEventListener("scroll", swipeHeader);
+    
 });
