beea14ff078610f5b9e533da66e480dcb10d8bcc
DiegNav33 <diegnavpro@gmail.com>
2023-08-26 22:12:50 +0200

    about page modified, new icon and img in few sections, scrpit js modified added a new function translate for the projects page, scss modified and html

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-
+    // START CHANGE HEADER ALL PAGES
     function swipeHeader() {
         const headerElt = document.querySelector("header");
         const headerLogoElt = document.querySelector("header img");
@@ -23,5 +23,32 @@ document.addEventListener("DOMContentLoaded", function() {
     };
 
     document.addEventListener("scroll", swipeHeader);
+    //END CHANGE HEADER ALL PAGES
+
+    // START TRANSLATE ON PROJECTS PAGE
+    function translateProjectsArticle(){
+        let articlesFromLeft = document.querySelectorAll(".articleLeft");
+        let articlesFromRight = document.querySelectorAll(".articleRight");
+
+        const {scrollTop, clientHeight} = this.document.documentElement; // destructuring -> creation deux const a partir de proprieté utile. a partir des proprieté de l' objet document.documentElement, maintenant acces a scrollTop (scroll depuis le top) et client height(hauteur de la partie visible de ma fenetre)
+
+        for (let articleLeft of articlesFromLeft) {
+            const topEltToTopViewport = articleLeft.getBoundingClientRect().top
+
+            if(scrollTop > (scrollTop + topEltToTopViewport).toFixed() - clientHeight * 0.80) {
+                articleLeft.classList.add("activeTranslateArticle");
+            }
+        };
+
+        for (let articleRight of articlesFromRight) {
+            const topEltToTopViewport = articleRight.getBoundingClientRect().top
+
+            if(scrollTop > (scrollTop + topEltToTopViewport).toFixed() - clientHeight * 0.80) {
+                articleRight.classList.add("activeTranslateArticle");
+            }
+        };
+    };
     
+    window.addEventListener("scroll", translateProjectsArticle);
+    // END TRANSLATE ON PROJECTS PAGE
 });
