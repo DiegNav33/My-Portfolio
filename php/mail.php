6aafa943ed1e55f9d89582d974edfc10d2b57df3
DiegNav33 <diegnavpro@gmail.com>
2023-11-17 14:36:57 +0100

    last edits done, on prod

--- php/mail.php
+++ php/mail.php
@@ -1,12 +1,10 @@
 <?php
-    var_dump($_POST);
 
      if(!empty($_POST)){
         
-         $formContent = $_POST["name"]."\n".$_POST["email"]."\n".$_POST["tel"]."\n".$_POST["message"];
-         $mailReturn = mail("diegnavpro@gmail.com", $_POST["subject"], $formContent,"");
+        $formContent = $_POST["name"]."\n".$_POST["email"]."\n".$_POST["tel"]."\n".$_POST["message"];
+        $mailReturn = mail("diegnavpro@gmail.com", $_POST["subject"], $formContent,"");
 
-         var_dump($mailReturn, $formContent);
      }
 
-    // header("Location:../pages/contact.html");
+    header("Location:../pages/contact.html");
