f9bafd2d00685c40b9321e99a022620ddb57d331
DiegNav33 <diegnavpro@gmail.com>
2023-11-19 01:15:05 +0100

    Accessibility modified, and mail function modified with template

--- php/mail.php
+++ php/mail.php
@@ -1,10 +1,43 @@
 <?php
 
-     if(!empty($_POST)){
-        
-        $formContent = $_POST["name"]."\n".$_POST["email"]."\n".$_POST["tel"]."\n".$_POST["message"];
-        $mailReturn = mail("diegnavpro@gmail.com", $_POST["subject"], $formContent,"");
+    if (!empty($_POST)) {
 
-     }
+      $recipientEmail = "diegnavpro@gmail.com";
+      $subject = $_POST["subject"];
+      $senderName = $_POST["name"];
+      $senderEmail = $_POST["email"];
+      $senderTel = $_POST["tel"];
+      $message = $_POST["message"];
+  
+      // Build the email content using a template
+      $emailTemplate = "
+          Hello,
+  
+          Someone has just contacted you. Here are the details:
+  
+          Name: $senderName
+          Email: $senderEmail
+          Phone: $senderTel
+  
+          Message:
+          $message
+      ";
+  
+      // Email headers
+      $headers = "From: $senderEmail\r\n";
+      $headers .= "Reply-To: $senderEmail\r\n";
+  
+      // Send the email
+      $mailReturn = mail($recipientEmail, $subject, $emailTemplate, $headers);
+  
+      // if ($mailReturn) {
 
-    header("Location:../pages/contact.html");
+      //     echo "The email has been sent successfully.";
+      // } 
+      // else {
+
+      //     echo "Error sending the email. Please try again.";
+      // }
+    }
+  
+  header("Location:../pages/contact.html");
