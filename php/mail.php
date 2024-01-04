e2555b466324e77eb95f237684c1f3f295cce0b9
DiegNav33 <diegnavpro@gmail.com>
2024-01-04 15:51:44 +0100

    ”copright”

--- php/mail.php
+++ php/mail.php
@@ -1,31 +1,31 @@
 <?php
 
-    if (!empty($_POST)) {
+  if (!empty($_POST)) {
+    
+    $recipientEmail = "diegnavpro@gmail.com";
+    $subject = $_POST["subject"];
+    $senderName = $_POST["name"];
+    $senderEmail = $_POST["email"];
+    $senderTel = $_POST["tel"];
+    $message = $_POST["message"];
 
-      $recipientEmail = "diegnavpro@gmail.com";
-      $subject = $_POST["subject"];
-      $senderName = $_POST["name"];
-      $senderEmail = $_POST["email"];
-      $senderTel = $_POST["tel"];
-      $message = $_POST["message"];
-  
-      // Build the email content using a template
-      $emailTemplate = "
-          Hello,
-  
-          Someone has just contacted you. Here are the details:
-  
-          Name: $senderName
-          Email: $senderEmail
-          Phone: $senderTel
-  
-          Message:
-          $message
-      ";
-  
-      // Send the email
-      $mailReturn = mail($recipientEmail, $subject, $emailTemplate,"");
-  
-    }
+    // Build the email content using a template
+    $emailTemplate = "
+      Hello,
+
+      Someone has just contacted you. Here are the details:
+
+      Name: $senderName
+      Email: $senderEmail
+      Phone: $senderTel
+
+      Message:
+      $message
+    ";
+
+    // Send the email
+    $mailReturn = mail($recipientEmail, $subject, $emailTemplate,"");
+
+  }
   
   header("Location:../pages/contact.html");
